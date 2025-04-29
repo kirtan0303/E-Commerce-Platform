cec29e9a6447fae1e218fe47a84323022afd74ba
kirtan0303 <117120542+kirtan0303@users.noreply.github.com>
2025-04-29 13:04:13 +0530

    Create app.js

---
+++ Backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Express app
+const app = express();
